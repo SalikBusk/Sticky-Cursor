8af635f785156369827672bc382010cdf79c06df
salikbubu <salikbula@gmail.com>
2024-04-10 15:37:00 +0200

    ....

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -11,18 +11,17 @@ const Card = forwardRef((props, ref) => {
       <Magnetic>
         {/* the stickyCursor should stick to the classes and not move */}
         <div
-          className="w-[380px] h-[50vh] rounded-xl p-2 overflow-hidden hover:border border-black"
+          className="w-[380px] h-[68vh] rounded-xl p-3 overflow-hidden hover:border border-black"
           ref={ref}
         >
           <div className="w-full h-full rounded-md group overflow-hidden relative">
             {/* Card Image */}
-            <div className="w-full h-[40vh] bg-pink-500 group-hover:h-full ease-in-out duration-300 rounded-md" />
+            <div className="w-full h-[58vh] bg-pink-500 group-hover:h-full ease-in-out duration-300 rounded-md" />
 
             {/* Card content */}
             <div className="pt-2 flex flex-row items-center justify-between">
               <div>
-                <p>{"[title]"}</p>
-                <p>{"[Description]"}</p>
+                <p className="text-3xl font-bold">{"[title]"}</p>
               </div>
               <Link
                 href={`/categori`}
